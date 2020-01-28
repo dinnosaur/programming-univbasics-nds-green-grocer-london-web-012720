48718ff297ebb99dc0f53e1e1f584860445bccef
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-28 12:58:14 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -121,7 +121,7 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
-consildated_cart = consolidate_cart(cart)
+consolidated_cart = consolidate_cart(cart)
 couponed_cart = apply_coupons(consolidated_cart, coupons)
 clearanced_cart = apply_clearance(couponed_cart)
 total = 0
