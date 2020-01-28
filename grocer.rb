3e6a888c5d6320331a4e1b6d2aa546b0e1f17f60
Dinno Roni Sivagnanam <dinnoroni@gmail.com>
2020-01-28 13:26:20 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -28,7 +28,7 @@ counter =0
 while counter < cart.length
   new_cart_item = find_item_by_name_in_collection(cart[counter][:item], new_cart)
   if new_cart_item != nil
-    new_cart_item += 1
+    new_cart_item[:count] += 1
   else
     new_cart_item =
     {
